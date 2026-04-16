2dd0cdd38fa09fd254d03266ae3e65d84a68fe9d
Guilherme Lima <guilherme.lima@sptech.school>
2026-04-16 09:20:42 -0300

    arrumando pastas, e nome da classe main

--- email_service/email_service/src/main/java/com/onepilates/email_service/domain/enums/TypeEmail.java
+++
@@ -1,9 +0,0 @@
-package com.onepilates.email_service.domain.enums;
-
-public enum TypeEmail {
-    PRIMEIRO_ACESSO,
-    CODIGO_ACESSO,
-    AULA_CRIADA,
-    AULA_ATUALIZADA,
-    AULA_CANCELADA
-}
--- email_service/email_service/src/main/java/com/onepilates/email_service/domain/gatway/EmailGatway.java
+++
@@ -1,6 +0,0 @@
-package com.onepilates.email_service.domain.gatway;
-
-public interface EmailGatway {
-     void enviarEmail(String destinatario, String assunto, String corpoHtml);
-
-}
